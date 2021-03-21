8ab27e1127c30d4cf9e855681fef480d256a940c
khan Tanveer <kt224945@gmail.com>
2021-03-21 15:50:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <h1>hello khan programmer so let's build instagram clone with react</h1>
+
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+
+      {/* Header  */}
+
+      {/*  posts */}
+      {/*  posts */}
     </div>
   );
 }
