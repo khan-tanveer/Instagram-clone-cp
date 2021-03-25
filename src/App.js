7ab9b4b09aa08288671bd33fb20f7a80284ce3d1
khan Tanveer <kt224945@gmail.com>
2021-03-25 21:34:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Post from "./Post";
 import { db, auth } from "./firebase";
 import { Button, Input, makeStyles, Modal } from "@material-ui/core";
 
-// modal styles
+// modal styles.................................................................................
 
 function getModalStyle() {
   const top = 50;
@@ -35,14 +35,15 @@ const App = () => {
   const classes = useStyles();
   const [modalStyle] = useState(getModalStyle);
   const [open, setOpen] = useState(false);
-
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+  const [openSignIn, setOpenSignIn] = useState(false);
+
   // for posts
 
-  // useEffect runs based on a specific condition
+  // useEffect runs based on a specific condition......................................
 
   useEffect(() => {
     // this is where the code runs
@@ -57,8 +58,11 @@ const App = () => {
     });
   }, []);
 
+  //authentication part..............................................
+  //userlogin and logout
+
   useEffect(() => {
-    auth.onAuthStateChanged((authUser) => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
       if (authUser) {
         // user has logged in
         console.log(authUser);
@@ -67,20 +71,51 @@ const App = () => {
         // user has logged out
         setUser(null);
       }
+
+      return () => {
+        //perform cleanup action
+        unsubscribe();
+      };
     });
-  }, []);
+  }, [user, username]);
+
+  // sign up....................................................................
 
   const signUp = (e) => {
     e.preventDefault();
 
     auth
       .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
       .catch((error) => alert(error.message));
+
+    setOpen(false);
+  };
+
+  // sign in........................./................................................
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .catch((error) => alert(error.message));
+
+    setOpenSignIn(false);
   };
 
   return (
     <div className="App">
-      {/* modal */}
+      {/* I want to have a.... */}
+      {/* caption input */}
+      {/* file picker */}
+      {/* postt button */}
+
+      {/* modal............................................................................. */}
 
       <Modal open={open} onClose={() => setOpen(false)}>
         <div style={modalStyle} className={classes.paper}>
@@ -93,7 +128,7 @@ const App = () => {
               />
             </center>
             <Input
-              placeholder="Enter a Email"
+              placeholder="Enter a username"
               type="text"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
@@ -115,7 +150,35 @@ const App = () => {
         </div>
       </Modal>
 
-      {/* Header  */}
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signUp">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="Enter a Email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="Enter a password"
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signIn}>Sign Ip</Button>
+          </form>
+        </div>
+      </Modal>
+
+      {/* Header ............................................................................................. */}
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -125,12 +188,19 @@ const App = () => {
       </div>
 
       {/* Button */}
-      <Button onClick={() => setOpen(true)}>sign up</Button>
+
+      {user ? (
+        <Button onClick={() => auth.signOut()}>logout</Button>
+      ) : (
+        <div className="app__loginContainer">
+          <Button onClick={() => setOpenSignIn(true)}>SIGN IN</Button>
+          <Button onClick={() => setOpen(true)}>SIGN UP</Button>
+        </div>
+      )}
 
       <h1>hello khan programmer so let's build instagram clone with react</h1>
 
-      {/*  posts */}
-
+      {/*  posts.......................................................................................... */}
       {posts.map(({ id, post }) => (
         <Post
           key={id}
